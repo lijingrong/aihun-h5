c068311c71957fd585fe595e7f46f2aa36ce94cb
hj200724 <huangjiang1026@163.com>
2018-04-23 17:45:10 +0800

    添加游戏首页场景，修改加载游戏场景

---
+++ assets/scripts/HomeGame.js
@@ -0,0 +1,76 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        speeker: {
+            default: null,
+            type: cc.Node
+        },
+        openSpeekerSpriteFrame: {
+            default: null,
+            type: cc.SpriteFrame
+        },
+        closeSpeekerSpriteFrame: {
+            default: null,
+            type: cc.SpriteFrame
+        },
+        singleBtn: {
+            default: null,
+            type: cc.Button
+        },
+        doubleBtn: {
+            default: null,
+            type: cc.Button
+        },
+        descBtn: {
+            default: null,
+            type: cc.Button
+        },
+        speekerBtn: {
+            default: null,
+            type: cc.Button
+        },
+        singleSceneName: "",
+        doubleSceneName: "",
+        descSceneName: "",
+        openSpeeker: true,
+    },
+
+    onLoad() {
+        this.openSpeeker = true;
+
+        this.singleBtn.node.on("click", function () {
+            this.switchScene(this.singleSceneName);
+        }, this);
+
+        this.doubleBtn.node.on("click", function () {
+            this.switchScene(this.doubleSceneName);
+        }, this);
+
+        this.descBtn.node.on("click", function () {
+            this.switchScene(this.descSceneName);
+        }, this);
+
+        this.speekerBtn.node.on("click", this.switchSpeeker, this);
+    },
+
+    switchScene: function (sceneName) {
+        // 切换场景
+        cc.director.loadScene(sceneName);
+    },
+
+    switchSpeeker: function () {
+        
+        // 切换喇叭图标
+        if (this.openSpeeker) {
+            this.speeker.SpriteFrame = this.closeSpeekerSpriteFrame;
+            this.openSpeeker = false;
+            // 关闭声音
+        } else {
+            this.speeker.SpriteFrame = this.openSpeekerSpriteFrame;
+            this.openSpeeker = true;
+            // 打开声音
+        }
+    },
+});
