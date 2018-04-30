410dfea2bcd1114711ae9030f062c65a06343550
hj200724 <huangjiang1026@163.com>
2018-04-30 14:48:17 +0800

    布丁和茶叶倒计时场景增肌滴滴音效

--- assets/scripts/countDown.js
+++ assets/scripts/countDown.js
@@ -3,15 +3,26 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        audio: {
+            default: null,
+            url: cc.AudioClip,
+        },
         time: 1, // 倒计时间隔时间
         loadNewScene: ""
     },
 
     onLoad () {
-        this.callback = function () {
+        if (this.audio !== null) {
+            this.didi = cc.audioEngine.play(this.audio, true, 1);
+        }
+
+        this.countDown = function () {
             cc.director.loadScene(this.loadNewScene);
         };
-        this.schedule(this.callback, this.time);
+        this.schedule(this.countDown, this.time);
     },
 
+    onDestroy: function () {
+        cc.audioEngine.stop(this.didi);
+    }
 });
