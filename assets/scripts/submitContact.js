35d44a87ae5a8dc7f2040959c9a336b241141637
hj200724 <huangjiang1026@163.com>
2018-05-07 17:37:24 +0800

    信息提交页面增加手机号格式验证

--- assets/scripts/submitContact.js
+++ assets/scripts/submitContact.js
@@ -22,17 +22,21 @@ cc.Class({
         var zhName = this.zhNameText.string;
         var telephone = this.telephoneText.string;
         var address = this.addressText.string;
-        if(zhName===null || telephone===null || address===null 
-            ||zhName.trim()==='' || telephone.trim()===''||address.trim()===''){
+        if (zhName === null || telephone === null || address === null
+            || zhName.trim() === '' || telephone.trim() === '' || address.trim() === '') {
             alert("请把信息填写完整！");
+            return;
         }
-        var url = "/aihun/addContact";
+        if (!telephone.match(/^(13[0-9]|14[0-9]|15[0-9]|16[6]|17[0-9]|18[0-9]|19[89])[0-9]{8}$/)) {
+            alert("手机号码格式不正确，请重新输入");
+            return;
+        }
+        var url = Config.domain + "/aihun/addContact";
         var params = "userId=" + encodeURIComponent(Config.uid) + "&zhName=" + encodeURIComponent(zhName) + "&telephone=" + encodeURIComponent(telephone) + "&address=" + encodeURIComponent(address);
         var request = cc.loader.getXMLHttpRequest();
         request.onreadystatechange = function () {
             if (request.readyState == 4 && (request.status >= 200 && request.status < 400)) {
                 var response = JSON.parse(request.responseText);
-                cc.log(response.code);
                 if (response.code === 1) {
                     cc.director.loadScene("contactSuccess");
                 }
